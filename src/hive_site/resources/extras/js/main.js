e465821defa1453eb32a00edb766c1889d837862
joamag <joamag@gmail.com>
2014-04-24 16:25:46 +0100

    new year

--- src/hive_site/resources/extras/js/main.js
+++ src/hive_site/resources/extras/js/main.js
@@ -1,5 +1,5 @@
 // Hive Solutions Website
-// Copyright (C) 2010-2012 Hive Solutions Lda.
+// Copyright (C) 2008-2014 Hive Solutions Lda.
 //
 // This file is part of Hive Solutions Website.
 //
@@ -17,7 +17,7 @@
 // __version__   = 1.0.0
 // __revision__  = $LastChangedRevision$
 // __date__      = $LastChangedDate$
-// __copyright__ = Copyright (c) 2010-2012 Hive Solutions Lda.
+// __copyright__ = Copyright (c) 2008-2014 Hive Solutions Lda.
 // __license__   = Hive Solutions Confidential Usage License (HSCUL)
 
 jQuery(document).ready(function() {
